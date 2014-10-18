84d2482283fdd2e1f1f0be60f818053d271f3c54
MiteyIronPaw <3twilding@gmail.com>
2014-10-18 13:03:30 +1300

    Create wrapper.py

---
+++ wrapper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+
+from minecraft_query import MinecraftQuery
+
+def main():
+    try:
+        query = MinecraftQuery("malomaxdew.brainswitch.se", 25565,
+                               timeout=10,
+                               retries=0)
+        server_data = query.get_rules()
+    except socket.error as e:
+        print "socket exception caught:", e.message
+        print "Server is down or unreachable."
+        sys.exit(1)
+    
+    print(server_data['motd'])
+    
+    print(server_data['version'])
+    		
+    NumPlayers = "Players "
+    NumPlayers += str(server_data['numplayers']) + "/"
+    NumPlayers += str(server_data['maxplayers']) + " :"
+    print(NumPlayers)
+    
+    Players = server_data['players']
+    for player in Players:
+    	print("\t"+player)
+    
+    sys.exit(0)
+
+
+if __name__=="__main__":
+    main()
